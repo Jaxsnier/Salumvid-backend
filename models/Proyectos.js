8bd361c465b358dff1924cdb41ca19e2d3c2e904
ELIGIO <eligioestevez@hotmail.com>
2025-09-17 22:41:59 -0400

    creando modelos db

---
+++ models/Proyectos.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Ventana = require('./Ventana');
+
+
+const Proyecto = mongoose.model('Proyecto',new mongoose.Schema({
+  name: String,
+  ventanas : Ventana,
+  
+}));
+
+module.exports = Proyecto;
---
+++ models/Ventana.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+const Ventana = mongoose.model('Ventana', new mongoose.Schema({
+    //<!-- Medidas Hueco -->
+    Id: String,
+    Hueco_Cant: String,
+    Hueco_Ancho: String,
+    Hueco_Alto: String,
+    Hueco_Hojas: String,
+    //<!-- Perfiles Marco -->
+    Perfiles_Marco_Cant: String,
+    Perfiles_Marco_Medida: String,
+    Perfiles_Marco_Cant: String,
+    Perfiles_Marco_Medida: String,
+    //<!-- Perfiles Hoja -->
+    Perfiles_Hoja_Cant: String,
+    Perfiles_Hoja_Medida: String,
+    Perfiles_Hoja_Cant: String,
+    Perfiles_Hoja_Medida: String,
+    //<!-- Vidrio -->
+    Viderio_Cant: String,
+    Viderio_Ancho: String,
+    Viderio_Alto: String,
+    //<!-- Goma -->
+    Goma_Pie: String,
+}));
+
+module.exports = Ventana;
