eddfbfa936010b40bbe9568044ab0524b7972c12
ELIGIO <eligioestevez@hotmail.com>
2025-09-23 16:37:11 -0400

    empezando a organizar codigo

---
+++ Tablas/Bd_tablas.js
@@ -0,0 +1,8 @@
+//buscar tabla en base de datos de proyecto
+async function BuscarTabla (  userId, proyecto_nombre ) {  //pedir tablas de datos
+
+   const proyecto = await Proyectos.findOne({ propietario: userId,proyecto_nombre: proyecto_nombre }).populate("arreglo_ventanas")
+
+   return res.send(proyecto);
+
+};
