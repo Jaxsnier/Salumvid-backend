f58e7abf91199dafca7a7d3312226ce01e816fc4
ELIGIO <eligioestevez@hotmail.com>
2025-09-26 21:54:25 -0400

    creando funciones de tablas

--- Tablas/Bd_tablas.js
+++ Tablas/Bd_tablas.js
@@ -1,16 +1,24 @@
 import Proyectos from "../models/Proyectos.js";
 
 //buscar tabla en base de datos de proyecto
-export async function BuscarTabla (  userId, proyecto_nombre ) {  //pedir tablas de datos
+export async function BuscarTabla(userId, proyecto_nombre) {  //pedir tablas de datos
 
-   const proyecto = await Proyectos.findOne({ propietario: userId,proyecto_nombre: proyecto_nombre }).populate("arreglo_ventanas")
+   const proyecto = await Proyectos.findOne({ propietario: userId, proyecto_nombre: proyecto_nombre }).populate("arreglo_ventanas")
 
    return proyecto
-
 };
 
-export const prueba = () =>
+export async function CrearTabla(params) 
 {
+   return null
+}
+
+export async function ActualizarTabla(params) 
+{
+   return null
+}
+
+export const prueba = () => {
    return "hola"
 }
 
